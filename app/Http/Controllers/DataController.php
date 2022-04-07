533d7d4116028d6571dc55a39b2ef554701b8ddc
amany-makhlof <62374159+amany-makhlof@users.noreply.github.com>
2022-04-07 14:22:23 +0200

    first commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Data;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class DataController extends Controller
+{
+    public function index()
+    {
+        $data = Data::all();
+        return view('welcome', ['data' => $data]);
+    }
+    public function store(Request $request)
+    {
+        $data = new Data;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->save();
+        return back();
+    }
+    public function generate($id)
+    {
+        $data = Data::findOrFail($id);
+        $qrcode = QrCode::size(400)->generate($data->name);
+        return view('qrcode', compact('qrcode'));
+    }
+}
